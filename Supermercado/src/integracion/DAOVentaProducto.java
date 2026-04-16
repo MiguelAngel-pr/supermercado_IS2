f6aeff3c1ab48d4c9ee83c537c0e5b07f4d322c4
MiguelAngel-pr <miguse04@ucm.es>
2026-04-16 17:21:22 +0200

    Revert "ventaPresentacion"
    This reverts commit 12ef24b86fc989ee3b83c42523a9d9814db8bec8.

--- Supermercado/src/integracion/DAOVentaProducto.java
+++
@@ -1,9 +0,0 @@
-package integracion;
-
-import java.util.Set;
-import negocio.TVentaProducto;
-
-public interface DAOVentaProducto {
-    int create(TVentaProducto tVentaProducto);
-    Set<TVentaProducto> readByVenta(int idVenta);
-}
--- Supermercado/src/presentacion/IGUI.java
+++
@@ -1,5 +0,0 @@
-package presentacion;
-
-public interface IGUI {
-    void update(GUIEvents event, Object data);
-}
